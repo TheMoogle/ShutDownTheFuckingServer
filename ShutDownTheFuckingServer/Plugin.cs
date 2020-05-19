4a29f552cbffe91f633597445e5c2adee6c67cb2
TheMoogle <34310114+TheMoogle@users.noreply.github.com>
2020-05-19 01:36:02 -0500

    version yes yes

---
+++ ShutDownTheFuckingServer/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using EXILED;
+using System.Diagnostics;
+
+namespace ShutDownTheFuckingServer
+{
+    public class Plugin : EXILED.Plugin
+    {
+        bool isEnabled;
+
+        public override void OnEnable()
+        {
+            LoadConfigs();
+
+            if (!isEnabled) return;
+
+            Log.Debug("Epic Server Shutdown/Bootloop brought to you by That Moogle!");
+            Log.Debug(":D");
+            Process.GetCurrentProcess().Kill();
+        }
+
+        public override void OnDisable()
+        {
+            Log.Debug(":)");
+        }
+
+        public override void OnReload()
+        {
+            Log.Debug("Why?");
+        }
+
+        private void LoadConfigs()
+        {
+            isEnabled = Config.GetBool("sdtfs_enabled", true);
+        }
+
+
+        public override string getName { get; } = "Shut Down The Fucking Server";
+    }
+}
